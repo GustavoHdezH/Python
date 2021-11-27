f40f280800a1f74ae17edb25b13c358f9c522ce6
ghernandez <ghernandez@cxc.com.mx>
2021-11-26 23:23:21 -0600

    Se mejora la descripcion del programa

--- Starter/number.py
+++ Starter/number.py
@@ -1,18 +1,29 @@
+"""
+Purpose
+    Demostrar el uso de ingreso basico de datos en un programa
+
+Running the code
+    py main.py
+
+Additional information
+    Adivina el numero en el menor numero de intentos
+"""
 import random
 
 guessesTaken = 0
 minNumber = 1
 maxNumber = 20
-print("""
-===============================================================
-                        NUMBER GUESSING
-===============================================================
-""")
+guees = None
+
+print("===============================================================")
+print("                        NUMBER GUESSING                        ")
+print("===============================================================")
+
 username = input("Enter your username ~> ")
 
 number = random.randint(minNumber, maxNumber)
-print("Well, " + username + " I am thinking in a number between " + str(number) 
-        + " and " + str(maxNumber))
+print("Well {} I am thinking in a number between {} and {} ".format
+        (username, str(number), str(maxNumber)))
 
 while guessesTaken < 6:
     guess = input("Take a guess ~> ")
@@ -30,9 +41,18 @@ while guessesTaken < 6:
 if guess == number:
     guessesTaken = str(guessesTaken)
     print("=====================================================")
-    print("$ Good job " + username + " you guessed my number in " + 
-            guessesTaken + " guesses")
+    print("$ Good job {} you guessed my number in {} guesses".format
+            (username, guessesTaken))
+
 if guess != number:
     number = str(number)
     print("=================================================")
-    print("$ No. the number i was thinking of was " + number)
+    print("$ No. the number i was thinking of was {}".format(number))
+
+"""Se define los parametros
+:param guessesTaken: Numero de intentos durante el juego
+:param minNumber: Numero minimo del rango de numeros para adivinar
+:param maxNumer: Numero maximo del rango de numeros para adivinar
+:param username: Nombre del usuario para el juego
+:param number: Numero generado por el programa
+"""
